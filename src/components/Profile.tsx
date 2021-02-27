5b6a635ae40c38390b46c8fe3608af35885555da
Rodrigo Almeida de Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2021-02-26 21:18:50 -0300

    aula 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/28880955?s=460&u=25a23d71f0442ed2401fcd768d58d97f073020b3&v=4" alt="Rodrigo" />
       <div>
         <strong>Rodrigo Magalhaes</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
         </p>
       </div>
     </div>
